c40dee738e7470b7a5bce3699358608b03521334
xlcaceres <lizxime4ever@hotmail.com>
2022-07-04 15:38:02 -0500

    Creacion Not found

--- apps/routes.py
+++ apps/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 routes = Blueprint('routes',__name__)
 
@@ -12,3 +12,6 @@ def dashboard():
     return render_template("home/dashboard.html")
 
 #Not found
+@routes.app_errorhandler(404)
+def error_404(error):
+    return render_template('home/notfound.html'), 404
