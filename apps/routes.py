094cb4037ffeb1f643214a7a92af24ca77a4449e
Nedu-ds <nestorduque.10@gmail.com>
2022-07-05 23:58:12 -0500

    Fix Index and Dashboard styles

--- apps/routes.py
+++ apps/routes.py
@@ -1,14 +1,20 @@
-from flask import Blueprint, render_template
+from urllib import response
+from urllib.request import Request
+from flask import Blueprint, redirect, render_template, request, make_response, session
 
 routes = Blueprint('routes',__name__)
 
 @routes.route('/')
 def home():
+    
     return render_template("home/index.html")
 
 
 @routes.route('/dashboard')
 def dashboard():
+    
+         
     return render_template("home/dashboard.html")
+
 
 #Not found
